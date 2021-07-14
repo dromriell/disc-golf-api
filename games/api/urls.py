17708e7f4df50f6a55568af4f4583b738adeadb4
dromriell <devonromriell@gmail.com>
2021-07-14 16:45:56 -0400

    - Set paths for scorecard and holescore endpoints

--- games/api/urls.py
+++ games/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers, urlpatterns
-from .views import GameViewSet
+from .views import GameViewSet, ScoreCardViewSet, HoleScoreViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'list', GameViewSet)
+router.register(r'scorecards', ScoreCardViewSet, basename='scorecards')
+router.register(r'hole_scores', HoleScoreViewSet, basename='hole_scores')
 
 urlpatterns = [
    path('', include(router.urls)),
