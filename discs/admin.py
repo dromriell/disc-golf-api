0180043ce4f5310181e5cb46e41c1bd0ea1e0074
dromriell <devonromriell@gmail.com>
2021-07-13 23:28:31 -0400

    - Add Manufacturer  admin with Disc Inline

--- discs/admin.py
+++ discs/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Disc
+from django.db import models
+from .models import Disc, Manufacturer
 
 # Register your models here.
+class DiscInline(admin.StackedInline):
+   model = Disc
+
+
+class ManufacturerAdmin(admin.ModelAdmin):
+   inlines = [
+      DiscInline
+   ]
+
+
+admin.site.register(Manufacturer, ManufacturerAdmin)
 admin.site.register(Disc)
 
