6d7628821b480d14115ac7dbfb5bb987350b3cdd
dromriell <devonromriell@gmail.com>
2021-07-14 16:46:41 -0400

    - Create ScoreCardViewSet - Create HoleScoreViewSet - Add authentication and permissions to views

--- games/api/views.py
+++ games/api/views.py
@@ -1,14 +1,57 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
 from ..models import Game, ScoreCard, HoleScore, Stroke
 from ..serializers import (
    GameSerializer,
    ScoreCardSerializer,
-   HoleScoreSerializer,
+   GameHoleScoreSerializer,
    StrokeSerializer
    )
 
 
+USER = get_user_model()
+
+
 class GameViewSet(ModelViewSet):
+   authentication_classes = [TokenAuthentication, SessionAuthentication]
+   permission_classes = [IsAuthenticated]
+
    queryset = Game.objects.all()
    serializer_class = GameSerializer
+
+
+class ScoreCardViewSet(ModelViewSet):
+   authentication_classes = [TokenAuthentication, SessionAuthentication]
+   permission_classes = [IsAuthenticated]
+
+   serializer_class = ScoreCardSerializer
+
+   def get_queryset(self):
+      req_user = self.request.user
+      spec_user = self.request.query_params.get('user', None)
+
+      if spec_user:
+         USER.objects.get(id=spec_user)
+         return ScoreCard.objects.filter(player=spec_user)
+   
+      return ScoreCard.objects.filter(player=req_user)
+
+
+class HoleScoreViewSet(ModelViewSet):
+   authentication_classes = [TokenAuthentication, SessionAuthentication]
+   permission_classes = [IsAuthenticated]
+
+   serializer_class = GameHoleScoreSerializer
+
+   def get_queryset(self):
+      req_user = self.request.user
+      spec_user = self.request.query_params.get('user', None)
+
+      if spec_user:
+         USER.objects.get(id=spec_user)
+         return HoleScore.objects.filter(score_card__player=spec_user)
+   
+      return HoleScore.objects.filter(score_card__player=req_user)
 
