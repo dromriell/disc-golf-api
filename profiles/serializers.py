66eef4148ab663a07d48c018389755e2800b2f2e
dromriell <devonromriell@gmail.com>
2021-07-20 12:34:09 -0400

    - Add last-game field to ProfileSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Profile
+from games.models import Game
+from games.serializers import LastGameSummarySerializer
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+   last_game = LastGameSummarySerializer(source='get_last_game')
    class Meta:
       model = Profile
       fields = '__all__'
