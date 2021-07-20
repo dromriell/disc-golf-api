e6638e8bff35f668502ddd5f29a991f7d5283c47
dromriell <devonromriell@gmail.com>
2021-07-20 12:26:03 -0400

    - Add auth classes and permissions to views

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,12 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from ..models import Course, Hole
 from ..serializers import CourseSerializer, HoleSerializer
 
 
 class CourseViewSet(ModelViewSet):
+   authentication_classes = [TokenAuthentication, SessionAuthentication]
+   permission_classes = [IsAuthenticated]
+
    queryset = Course.objects.all()
    serializer_class = CourseSerializer
 
 class HoleViewSet(ModelViewSet):
+   authentication_classes = [TokenAuthentication, SessionAuthentication]
+   permission_classes = [IsAuthenticated]
+   
    queryset = Hole.objects.all()
    serializer_class = HoleSerializer
