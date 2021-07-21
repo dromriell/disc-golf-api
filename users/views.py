7e3b40a6d374f0f58fd857b1043aeba2291cf3f3
dromriell <devonromriell@gmail.com>
2021-07-20 22:54:16 -0400

    - Refactor code to add profile field in response

--- users/views.py
+++ users/views.py
@@ -2,9 +2,20 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
+from profiles.serializers import ProfileSerializer
+
 
 class CustomObtainAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
-        response = super(CustomObtainAuthToken, self).post(request, *args, **kwargs)
-        token = Token.objects.get(key=response.data['token'])
-        return Response({'token': token.key, 'user_id': token.user_id})
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        profile = ProfileSerializer(user.user_profile)
+        print(profile)
+        return Response({
+            'token': token.key, 
+            'user_id': user.id,
+            'profile': profile.data,
+            })
