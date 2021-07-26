5af57e95dc653966b518e0645c07cb671ea0bf08
dromriell <devonromriell@gmail.com>
2021-07-25 23:56:21 -0400

    - Refactor to use updated serializer for last game

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 from .models import Profile
 from games.models import Game
-from games.serializers import LastGameSummarySerializer
+from games.serializers import GameSummarySerializer
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-   last_game = LastGameSummarySerializer(source='get_last_game')
+   last_game = GameSummarySerializer(source='get_last_game')
    class Meta:
       model = Profile
       fields = '__all__'
