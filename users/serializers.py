35af3e072ec00540c3a4cf048cbb9c85695b95ee
dromriell <devonromriell@gmail.com>
2021-08-01 17:01:48 -0400

    + Create basic serializer for custom user model + Add custom create method to hash passwords and create user Token

---
+++ users/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+USER = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = USER
+      fields = '__all__'
+
+   def create(self, validated_data):
+    password = validated_data.pop('password')
+    user = super().create(validated_data)
+    user.set_password(password)
+    user.save()
+    Token.objects.get_or_create(user=user)
+    return user
+
