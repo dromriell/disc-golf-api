6b42d0c04b7fa596d18045536d4cc14ece67ee4e
dromriell <devonromriell@gmail.com>
2021-08-01 16:57:56 -0400

    + Create custom manager to handle new users + Add STATE_CHOICE options to User + Add required state field

--- users/models.py
+++ users/models.py
@@ -1,9 +1,82 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, password=None):
+        user = self.model()
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, password=None):
+        user = self.create_user(password=password,)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractUser):
+    STATE_CHOICES = [ 
+        ('AL', 'Alabama'), 
+        ('AK', 'Alaska'), 
+        ('AZ', 'Arizona'), 
+        ('AR', 'Arkansas'),  
+        ('CA', 'California'), 
+        ('CO', 'Colorado'), 
+        ('CT', 'Connecticut'), 
+        ('DE', 'Delaware'), 
+        ('DC', 'District of Columbia'), 
+        ('FL', 'Florida'), 
+        ('GA', 'Georgia'), 
+        ('HI', 'Hawaii'), 
+        ('ID', 'Idaho'), 
+        ('IL', 'Illinois'), 
+        ('IN', 'Indiana'), 
+        ('IA', 'Iowa'), 
+        ('KS', 'Kansas'), 
+        ('KY', 'Kentucky'), 
+        ('LA', 'Louisiana'), 
+        ('ME', 'Maine'), 
+        ('MD', 'Maryland'), 
+        ('MA', 'Massachusetts'), 
+        ('MI', 'Michigan'), 
+        ('MN', 'Minnesota'), 
+        ('MS', 'Mississippi'), 
+        ('MO', 'Missouri'), 
+        ('MT', 'Montana'), 
+        ('NE', 'Nebraska'), 
+        ('NV', 'Nevada'), 
+        ('NH', 'New Hampshire'), 
+        ('NJ', 'New Jersey'), 
+        ('NM', 'New Mexico'), 
+        ('NY', 'New York'), 
+        ('NC', 'North Carolina'), 
+        ('ND', 'North Dakota'),  
+        ('OH', 'Ohio'), 
+        ('OK', 'Oklahoma'), 
+        ('OR', 'Oregon'), 
+        ('PA', 'Pennsylvania'), 
+        ('RI', 'Rhode Island'), 
+        ('SC', 'South Carolina'), 
+        ('SD', 'South Dakota'), 
+        ('TN', 'Tennessee'), 
+        ('TX', 'Texas'), 
+        ('UT', 'Utah'), 
+        ('VT', 'Vermont'), 
+        ('VA', 'Virginia'), 
+        ('WA', 'Washington'), 
+        ('WV', 'West Virginia'), 
+        ('WI', 'Wisconsin'), 
+        ('WY', 'Wyoming')
+    ]
     birth_day = models.DateField(blank=True, null=True)
+    state = models.CharField(max_length=2, choices=STATE_CHOICES, default='KY')
+
+    objects = UserManager()
 
     def __str__(self):
         return self.username
+
+    
