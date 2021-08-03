d0aaa247e2517d7cce28050fb36f81cc73ddce1b
dromriell <devonromriell@gmail.com>
2021-08-03 13:51:12 -0400

    - Add UserDisc inline to ProfileAdmin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Profile
+from discs.models import UserDisc
+
+class UserDiscInline(admin.TabularInline):
+   model = UserDisc
+
+class ProfileAdmin(admin.ModelAdmin):
+   inlines = [
+      UserDiscInline
+   ]
 
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
