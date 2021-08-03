d6a35bfabe0cb6b9c14f28841b31a3a0db200ee7
dromriell <devonromriell@gmail.com>
2021-08-03 13:49:41 -0400

    + Create UserDiscSerializer

--- discs/serializers.py
+++ discs/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from .models import Disc
+from .models import Disc, UserDisc
 
 class DiscSerializer(serializers.ModelSerializer):
    class Meta:
       model = Disc
+      fields = '__all__'
+
+
+class UserDiscSerializer(serializers.ModelSerializer):
+   disc = DiscSerializer()
+   class Meta:
+      model = UserDisc
       fields = '__all__'
 
 
