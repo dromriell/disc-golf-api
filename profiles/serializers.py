dc34be77b14c83e45f85d19cf9b09f001d43f69a
dromriell <devonromriell@gmail.com>
2021-08-03 13:51:36 -0400

    - Add disc_bag field to ProfileSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 from .models import Profile
 from games.models import Game
 from games.serializers import GameSummarySerializer
+from discs.serializers import UserDiscSerializer
 
 
 class ProfileSerializer(serializers.ModelSerializer):
    last_game = GameSummarySerializer(source='get_last_game')
+   disc_bag = UserDiscSerializer(many=True)
    class Meta:
       model = Profile
       fields = '__all__'
