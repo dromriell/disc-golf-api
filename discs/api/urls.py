99c51b09d315bf1ab9a1578c294461ef2425705d
dromriell <devonromriell@gmail.com>
2021-08-10 16:29:42 -0400

    - Create endpoint for UserDiscViewSet

--- discs/api/urls.py
+++ discs/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers, urlpatterns
-from .views import DiscViewSet, DiscSearchViewSet
+from .views import DiscViewSet, UserDiscViewSet, DiscSearchViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'list', DiscViewSet)
+router.register(r'user-discs', UserDiscViewSet, basename='user discs')
 router.register(r'search', DiscSearchViewSet, basename='disc search')
 
 urlpatterns = [
