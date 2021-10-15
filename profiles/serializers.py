6e344c29245f21d7b1edac0aa9bb2ca44c2fc4d7
dromriell <devonromriell@gmail.com>
2021-10-14 21:44:35 -0400

    - Add UserProfileSerializer to user field

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,10 +3,12 @@ from .models import Profile
 from games.models import Game
 from games.serializers import GameSummarySerializer
 from discs.serializers import UserDiscSerializer
+from users.serializers import UserProfileSerializer
 
 
 class ProfileSerializer(serializers.ModelSerializer):
    last_game = GameSummarySerializer(source='get_last_game')
+   user = UserProfileSerializer()
    disc_bag = UserDiscSerializer(many=True)
    class Meta:
       model = Profile
